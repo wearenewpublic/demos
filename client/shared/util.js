956eefc51894b49befb8d47bc30b58c115ddb533
robennals <rob@ennals.org>
2023-05-04 11:42:25 -0700

    expandDataList adds a time stamp that messages can be sorted by.

--- client/shared/util.js
+++ client/shared/util.js
@@ -1,9 +1,14 @@
 
 export function expandDataList(list) {
+    const date = new Date();
+    date.setHours(date.getHours() - 1);
+
     return list.map((item, index) => {
+        date.setMinutes(date.getMinutes + 1);
         return {
             ...item,
             key: index,
+            time: date.getTime()
         };
     });
 }
