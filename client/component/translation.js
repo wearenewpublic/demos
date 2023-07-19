7e3be9f4535be73505f68b12e36ac8cef915d3f3
robennals <rob@ennals.org>
2023-07-18 17:44:41 -0700

    Added file missing from last commit.

---
+++ client/component/translation.js
@@ -0,0 +1,42 @@
+import { useContext } from "react";
+import { ui_translations_french } from "../translations/french/ui_french";
+import { ui_translations_german } from "../translations/german/ui_german";
+import { PrototypeContext } from "../organizer/PrototypeContext";
+import { Text } from "react-native";
+import { formatString } from "../util/util";
+
+export const languageEnglish = 'english';
+export const languageGerman = 'german';
+export const languageFrench = 'french';
+
+const ui_translations_for_language = {
+    german: ui_translations_german,
+    french: ui_translations_french
+}
+
+function translateText({text, language, formatParams}) {
+    const translations = ui_translations_for_language[language];
+    var translatedText = translations ? translations[text] : text;
+
+    if (!translatedText && language != languageEnglish && language) {
+        console.log('No translation for ' + text + ' in ' + language);
+    }
+    if (formatParams) {
+        translatedText = formatString(translatedText || text, formatParams);
+    }
+    return translatedText || text;
+}
+
+export function useTranslation(text, formatParams) {
+    const {instance} = useContext(PrototypeContext);
+    const language = instance?.language;
+    return translateText({text, language, formatParams});
+}
+
+export function TranslatableText({text, formatParams, style, ...props}) {
+    const translatedText = useTranslation(text, formatParams);
+    return <Text style={style} {...props}>{translatedText || text}</Text>
+}
+
+
+
