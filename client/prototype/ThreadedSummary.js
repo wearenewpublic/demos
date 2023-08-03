6e40865c5d1b31651b8d086c82f541c9044506c3
robennals <rob@ennals.org>
2023-08-03 14:59:27 -0700

    Added "Split" mode to ThreadedSummary. Gives Agree and Disagree points.

--- client/prototype/ThreadedSummary.js
+++ client/prototype/ThreadedSummary.js
@@ -25,46 +25,64 @@ export const ThreadedSummaryPrototype = {
         {key: 'wars', name: 'Star Wars vs Star Trek', 
         summary: 'Both are good',
             comment: expandDataList(trek_vs_wars)},
+        {key: 'wars-split', name: 'Star Wars vs Star Trek (Split)', 
+            split: true,
+                comment: expandDataList(trek_vs_wars)},
+    
     ],
     liveInstance: [
         {key: 'live', name: 'Live Conversation', comment: {}}
     ],
-    newInstanceParams: []    
+    newInstanceParams: [
+        {key: 'split', name: 'Split', type: 'boolean', default: false}
+    ]    
+}
+
+function SummaryEditor({property, name, prompt}) {
+    const value = useGlobalProperty(property);
+    const datastore = useDatastore();
+    const [inProgress, setInprogress] = useState(false);
+    const comments = useCollection('comment', {sortBy: 'time', reverse: true});
+
+    async function computeSummary() {
+        const commentsJSON = JSON.stringify(comments);
+        setInprogress(true);
+        const newSummary = await callServerApiAsync('chatgpt', 'chat', {promptKey: prompt, params: {commentsJSON}});
+        datastore.setGlobalProperty(property, newSummary);
+        setInprogress(false);
+    }
+
+    return <Card>
+        <SmallTitleLabel label={name} />
+        <Pad/>
+        <EditableText value={value || ''} onChange={newValue => datastore.setGlobalProperty(property,newValue)} 
+            placeholder={'Enter ' + name} />
+        <Pad/>
+        {inProgress ?
+            <QuietSystemMessage label='Computing...' />
+        :
+            <PrimaryButton label={'Generate New ' + name} onPress={computeSummary} />
+        }
+    </Card>
 }
 
 export function ThreadedSummaryScreen() {
     const comments = useCollection('comment', {sortBy: 'time', reverse: true});
     const topLevelComments = comments.filter(comment => !comment.replyTo);
-    const summary = useGlobalProperty('summary');
-    const datastore = useDatastore();
-    const [inProgress, setInprogress] = useState(false);
-
-    async function computeSummary() {
-        console.log('computeSummary', comments);
-        const commentsJSON = JSON.stringify(comments);
-        setInprogress(true);
-        const newSummary = await callServerApiAsync('chatgpt', 'chat', {promptKey: 'summary', params: {commentsJSON}});
-        console.log('newSummary', newSummary);
-        datastore.setGlobalProperty('summary', newSummary);
-        setInprogress(false);
-    }
+    const split = useGlobalProperty('split');
 
     return (
         <WideScreen pad>
             <ScrollView>
                 <Narrow>
-                    <Card>
-                        <SmallTitleLabel label="Summary" />
-                        <Pad/>
-                        <EditableText value={summary} onChange={newSummary => datastore.setGlobalProperty('summary', 
-                            newSummary)} placeholder='Enter summary' />
-                        <Pad/>
-                        {inProgress ?
-                            <QuietSystemMessage label='Computing summary' />
-                        :
-                            <PrimaryButton label="Generate New Conversation Summary" onPress={computeSummary} />
-                        }
-                    </Card>
+                    {split ? 
+                        <View>
+                            <SummaryEditor property='agree' name='Points of Agreement' prompt='agree_points' />
+                            <SummaryEditor property='disagree' name='Points of Disagreement' prompt='disagree_points' />
+                        </View>
+                    :
+                        <SummaryEditor property='summary' name='Conversation Summary' prompt='summary' />
+                    }
                 </Narrow>
                 <Center>
                     <View>
@@ -74,6 +92,7 @@ export function ThreadedSummaryScreen() {
                     )}
                     </View>
                 </Center>
+                <Pad size={24}/>
             </ScrollView>
         </WideScreen>
     )
